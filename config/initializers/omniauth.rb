97348e50aa26e1894cba4759d8b0ce0f73a9eb2f
Jeremy Mouton <jeremy@aspendigital.com>
2012-12-23 15:33:54 -0700

    Edited homepage, added iphone icons, removed mutual flirt nav tab.

--- config/initializers/omniauth.rb
+++
@@ -1,9 +0,0 @@
-# OmniAuth for user account authentication
-# http://railscasts.com/episodes/241-simple-omniauth-revised
-
-# OmniAuth.config.logger = Rails.logger
-
-# Rails.application.config.middleware.use OmniAuth::Builder do
-#   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
-#   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
-# end
